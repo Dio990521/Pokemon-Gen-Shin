1fa405032dc1932961ab6b8bfafdd48a3f1b6201
Dio990521 <55007715+Dio990521@users.noreply.github.com>
2023-06-08 16:42:59 +0900

    Add random wild pokemons

--- Assets/Scripts/Gameplay/MapArea.cs
+++ Assets/Scripts/Gameplay/MapArea.cs
@@ -1,15 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class MapArea : MonoBehaviour
 {
-    [SerializeField] private List<Pokemon> wildPokemons;
+    [SerializeField] private List<PokemonEncounterRecord> wildPokemons;
+
+    private void Start()
+    {
+        int totalChance = 0;
+        foreach (var record in wildPokemons)
+        {
+            record.chanceLower = totalChance;
+            record.chanceUpper = totalChance + record.chancePercentage;
+
+            totalChance += record.chancePercentage;
+        }
+    }
 
     public Pokemon GetRandomWildPokemon()
     {
-        Pokemon wildPokemon = wildPokemons[Random.Range(0, wildPokemons.Count)];
+        int randVal = Random.Range(1, 101);
+        var pokemonRecord = wildPokemons.First(p => randVal >= p.chanceLower && randVal <= p.chanceUpper);
+        var levelRange = pokemonRecord.levelRange;
+        int level = levelRange.y == 0 ? levelRange.x : Random.Range(levelRange.x, levelRange.y + 1);
+
+        var wildPokemon = new Pokemon(pokemonRecord.pokemon, level);
         wildPokemon.Init();
         return wildPokemon;
     }
+}
+
+[System.Serializable]
+public class PokemonEncounterRecord
+{
+    public PokemonBase pokemon;
+    public Vector2Int levelRange;
+    public int chancePercentage;
+
+    public int chanceLower { get; set; }
+    public int chanceUpper { get; set; }
 }
