6bb14198774b020f0bce3ff3f03fea668fa31c9d
Dio990521 <55007715+Dio990521@users.noreply.github.com>
2023-07-22 17:28:56 +0900

    add sand tower

--- Assets/Scripts/Gameplay/Lock.cs
+++ Assets/Scripts/Gameplay/Lock.cs
@@ -5,27 +5,64 @@ using UnityEngine;
 
 public class Lock : MonoBehaviour, InteractableObject, ISavable
 {
-    [SerializeField] private ItemBase _key;
+    [SerializeField] private ItemBase _keyItem;
+    [SerializeField] private int _total;
+    private int _cur;
     private Animator _animator;
     private bool _unlock;
 
     private void Awake()
     {
-        _animator = GetComponent<Animator>();
+        _animator = GetComponentInChildren<Animator>();
         _unlock = false;
+        _cur = 0;
+        GameEventManager.Instance.AddEventListener("SandTower", CheckUnlock);
     }
+
+    private void OnDestroy()
+    {
+        GameEventManager.Instance.RemoveEvent("SandTower", CheckUnlock);
+    }
+
+
     public IEnumerator Interact(Transform initiator)
     {
         Inventory inventory = initiator.gameObject.GetComponent<Inventory>();
-        if (inventory.HasItem(_key))
+        if (_keyItem != null )
         {
-            yield return DialogueManager.Instance.ShowDialogueText("使用了火箭队的钥匙解开了锁。");
-            _animator.SetBool("unlock", true);
-            Destroy(gameObject, 1f);
+            if (inventory.HasItem(_keyItem))
+            {
+                yield return DialogueManager.Instance.ShowDialogueText("使用了火箭队的钥匙解开了锁。");
+                UnlockAnim();
+            }
+            else
+            {
+                yield return DialogueManager.Instance.ShowDialogueText("神奇的锁，仿佛将世界与后面的空间隔绝开来。");
+            }
         }
         else
         {
-            yield return DialogueManager.Instance.ShowDialogueText("神奇的锁，仿佛将山洞后的空间隔绝开来。");
+            yield return DialogueManager.Instance.ShowDialogueText("神奇的锁，仿佛将世界与后面的空间隔绝开来。");
+        }
+
+
+    }
+
+    private void UnlockAnim()
+    {
+        _animator.SetBool("unlock", true);
+        Destroy(gameObject, 1f);
+    }
+
+    private void CheckUnlock()
+    {
+        if (_cur == _total-1)
+        {
+            UnlockAnim();
+        }
+        else
+        {
+            ++_cur;
         }
     }
 
---
+++ Assets/Scripts/Gameplay/SandTowerSwitch.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandTowerSwitch : MonoBehaviour, InteractableObject, ISavable
+{
+    [SerializeField] private ItemBase _keyItem;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    private bool _isActive;
+
+    private void Awake()
+    {
+        _isActive = false;
+    }
+
+    public IEnumerator Interact(Transform initiator)
+    {
+        if (!_isActive)
+        {
+            Inventory inventory = initiator.gameObject.GetComponent<Inventory>();
+
+            if (inventory.HasItem(_keyItem))
+            {
+                yield return DialogueManager.Instance.ShowDialogueText($"将{_keyItem.ItemName}放在了壁槽内。");
+                _isActive = true;
+                _spriteRenderer.sprite = _keyItem.Icon;
+                GameEventManager.Instance.CallEvent("SandTower");
+            }
+            else
+            {
+                yield return DialogueManager.Instance.ShowDialogueText("似乎可以放置某种东西的壁槽。");
+            }
+        }
+        else
+        {
+            yield return DialogueManager.Instance.ShowDialogueText($"{_keyItem.ItemName}静静地嵌在了壁槽内部。");
+        }
+    }
+
+    public object CaptureState()
+    {
+        return _isActive;
+    }
+
+    public void RestoreState(object state)
+    {
+        _isActive = (bool)state;
+        if (_isActive )
+        {
+            _spriteRenderer.sprite = _keyItem.Icon;
+        }
+    }
+
+
+}
